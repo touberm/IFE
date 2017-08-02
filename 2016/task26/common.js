6d293ad21448d0ca371a14477230a455c666e7c2
by <baiyang1114@163.com>
2017-08-02 17:25:18 +0800

    飞船与行星 结构页面DONE

---
+++ 2016/task26/common.js
@@ -0,0 +1,25 @@
+/* 
+ * @description:
+ * @author:
+ * @update:
+ */
+
+
+/*  */
+/* Function.prototype.addMethod = function(name,fn){
+  this.prototype[name] = fn;
+}
+
+var Methods  = function(){};
+Methods.addMethod('checkName',function(){
+  console.log('Name');
+}).addMethod('checkEmail',function(){
+  console.log('Email');
+});
+ */
+/* 调用 */
+/* var m = new Methods();
+m.checkEmail(); */
+
+/* END test */
+
